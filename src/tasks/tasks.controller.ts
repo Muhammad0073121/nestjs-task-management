228c62d05354828dc3d2fd589f4466a4cbbef2f2
Muhammad Abdullah <mabdullah3121@gmail.com>
2024-09-19 16:52:13 +0500

    createTaskDto added

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+import { createTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: createTaskDto): Task {
+    return this.taskService.createTask(createTaskDto);
+  }
+}
